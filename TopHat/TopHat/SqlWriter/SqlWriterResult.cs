a7ebbd56a8cd6f5c78c7ff21d9dfd161e3221798
markjerz <mark@jerzykowski.me.uk>
2014-05-01 15:39:02 +0100

    Added DatabaseEngine plus made internals visible to test

---
+++ TopHat/TopHat/SqlWriter/SqlWriterResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopHat.SqlWriter
+{
+    internal struct SqlWriterResult
+    {
+        public string Sql { get; set; }
+
+        public dynamic Parameters { get; set; }
+    }
+}
