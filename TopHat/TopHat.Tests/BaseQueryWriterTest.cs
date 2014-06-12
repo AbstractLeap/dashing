e133a9d45baf1821d0f6497254a213b7dda493cd
James Holwell <jamesholwell@me.com>
2014-06-12 13:45:53 +0100

    Removed Fakes (not in use) Removed LinFu (not in use) Removed other unreferenced assemblies

--- TopHat/TopHat.Tests/BaseQueryWriterTest.cs
+++ TopHat/TopHat.Tests/BaseQueryWriterTest.cs
@@ -2,15 +2,12 @@
     using System;
     using System.Data;
 
-    using Microsoft.QualityTools.Testing.Fakes;
-
     using Moq;
 
     using TopHat.CodeGeneration;
     using TopHat.Configuration;
-    using TopHat.Engine;
 
-    public class BaseQueryWriterTest : IDisposable {
+    public class BaseQueryWriterTest {
         protected readonly Mock<IDbConnection> Connection;
 
         protected readonly Mock<IDbTransaction> Transaction;
@@ -26,7 +23,6 @@
             this.Connection.Setup(c => c.State).Returns(ConnectionState.Open);
             this.Transaction = new Mock<IDbTransaction>(MockBehavior.Strict);
             this.CodeManager = new Mock<IGeneratedCodeManager>();
-            ////this.shimsContext = ShimsContext.Create();
             this.config = new Mock<IConfiguration>();
         }
 
@@ -34,10 +30,6 @@
             // Dapper.Fakes.ShimSqlMapper.ExecuteIDbConnectionStringObjectIDbTransactionNullableOfInt32NullableOfCommandType = (connection, SqlWriter, parameters, transaction, timeout, type) => 1;
             var session = new Session(this.Connection.Object, this.config.Object, this.Transaction.Object);
             return session;
-        }
-
-        public void Dispose() {
-            ////this.shimsContext.Dispose();
         }
     }
 }
