a34301776819925f257ada8e9e60db7c8a3c6a2e
Mark Jerzykowski <mark@jerzykowski.me.uk>
2014-06-25 23:40:02 +0100

    Well, it returns some tables

---
+++ TopHat/Configuration/ReverseEngineer/IEngineer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatabaseSchemaReader;
+
+namespace TopHat.Configuration.ReverseEngineer
+{
+    public interface IEngineer
+    {
+        IEnumerable<IMap> ReverseEngineer(IDatabaseReader databaseReader);
+    }
+}
