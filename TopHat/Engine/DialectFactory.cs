02e75e0cd7049b51e4fc34c7aa1ce3fa28942bdd
Mark Jerzykowski <mark@jerzykowski.me.uk>
2014-06-28 11:28:05 +0100

    Added Dialect Factory

---
+++ TopHat/Engine/DialectFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopHat.Engine
+{
+    public class DialectFactory
+    {
+        public ISqlDialect Create(string providerName)
+        {
+            switch (providerName)
+            {
+                case "System.Data.SqlClient":
+                    return new SqlServerDialect();
+
+                case "MySql.Data.MySqlClient":
+                    return new MySqlDialect();
+
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
