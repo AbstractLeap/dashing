07be1c58bed581b1ff1a211dd8734b53f726d4b9
James Holwell <jamesholwell@me.com>
2014-07-04 17:34:01 +0100

    Added tests around get-only properties (but actually it was solved by using !IsIgnored)

--- Dashing.Tests/CodeGeneration/ProxyGeneratorTests.cs
+++ Dashing.Tests/CodeGeneration/ProxyGeneratorTests.cs
@@ -1,7 +1,10 @@
 ﻿namespace Dashing.Tests.CodeGeneration {
     using System;
+    using System.CodeDom;
     using System.Collections.Generic;
     using System.Linq;
+
+    using Dashing.Tests.Annotations;
 
     using Microsoft.AspNet.Identity;
 
@@ -41,6 +44,42 @@
             Assert.Contains(typeof(IUser<int>).Assembly.Location, results.ReferencedAssemblyLocations);
         }
 
+        [Fact]
+        public void IgnoreGetOnlyPropertiesInChangeTrackingProxy() {
+            // assemble
+            var target = MakeTarget();
+            var maps = MakeMaps(typeof(ClassWithGetOnlyProperty));
+            var config = new CodeGeneratorConfig();
+
+            // act
+            var results = target.GenerateProxies(config, maps);
+
+            // assert
+            Assert.NotNull(results);
+            var changeTrackingProxy = results.ProxyTypes.Single(ctd => ctd.Name == typeof(ClassWithGetOnlyProperty).Name + config.TrackedClassSuffix);
+            var members = new CodeTypeMember[changeTrackingProxy.Members.Count];
+            changeTrackingProxy.Members.CopyTo(members, 0);
+            Assert.False(members.Any(m => m.Name == "Id"));
+        }
+
+        [Fact]
+        public void IgnoreGetOnlyPropertiesInUpdateProxy() {
+            // assemble
+            var target = MakeTarget();
+            var maps = MakeMaps(typeof(ClassWithGetOnlyProperty));
+            var config = new CodeGeneratorConfig();
+
+            // act
+            var results = target.GenerateProxies(config, maps);
+
+            // assert
+            Assert.NotNull(results);
+            var updateProxy = results.ProxyTypes.Single(ctd => ctd.Name == typeof(ClassWithGetOnlyProperty).Name + config.UpdateClassSuffix);
+            var members = new CodeTypeMember[updateProxy.Members.Count];
+            updateProxy.Members.CopyTo(members, 0);
+            Assert.False(members.Any(m => m.Name == "Id"));
+        }
+
         private static Dictionary<Type, IMap> MakeMaps(params Type[] types) {
             var mapper = MakeMapper();
             var maps = types.ToDictionary(t => t, mapper.MapFor);
@@ -59,6 +98,20 @@
             public int Id { get; private set; }
 
             public string UserName { get; set; }
+        }
+
+        // make R# relax!
+        // ReSharper disable once ClassWithVirtualMembersNeverInherited.Local
+        // ReSharper disable once MemberCanBeProtected.Global
+        private class ClassWithGetOnlyProperty {
+            public virtual int UserId { get; [UsedImplicitly] set; }
+
+            [UsedImplicitly]
+            public virtual int Id {
+                get {
+                    return this.UserId;
+                }
+            }
         }
     }
 }
