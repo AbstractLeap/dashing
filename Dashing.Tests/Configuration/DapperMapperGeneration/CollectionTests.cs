9abb9f0c617a0907d99070ec1d5b518eeca52c46
Mark Jerzykowski <mark@jerzykowski.me.uk>
2014-07-09 08:03:54 +0100

    Fixed SingleCollection mapping test

--- Dashing.Tests/Configuration/DapperMapperGeneration/CollectionTests.cs
+++ Dashing.Tests/Configuration/DapperMapperGeneration/CollectionTests.cs
@@ -1,39 +1,66 @@
 ﻿namespace Dashing.Tests.Configuration.DapperMapperGeneration {
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using Dashing.CodeGeneration;
+    using Dashing.Configuration;
+    using Dashing.Engine.DapperMapperGeneration;
+    using Dashing.Engine.Dialects;
+    using Dashing.Engine.DML;
+    using Dashing.Tests.TestDomain;
+    using Moq;
+    using Xunit;
+
+
     public class CollectionTests {
-        ////[Fact]
-        ////public void SingleCollectionWorks() {
-        ////    var funcFac = GenerateSingleMapper();
-        ////    var post1 = new Post { PostId = 1 };
-        ////    var post2 = new Post { PostId = 2 };
-        ////    var comment1 = new Comment { CommentId = 1 };
-        ////    var comment2 = new Comment { CommentId = 2 };
-        ////    var comment3 = new Comment { CommentId = 3 };
-        ////    var dict = new Dictionary<object, Post>();
-        ////    var func = (Func<Post, Comment, Post>)funcFac(dict);
-        ////    func(post1, comment1);
-        ////    func(post1, comment2);
-        ////    func(post2, comment3);
-        ////    Assert.Equal(1, dict[1].Comments.First().CommentId);
-        ////    Assert.Equal(2, dict[1].Comments.Last().CommentId);
-        ////    Assert.Equal(3, dict[2].Comments.First().CommentId);
-        ////}
+        [Fact]
+        public void SingleCollectionWorks() {
+            var funcFac = GenerateSingleMapper();
+            var post1 = new Post { PostId = 1 };
+            var post2 = new Post { PostId = 2 };
+            var comment1 = new Comment { CommentId = 1 };
+            var comment2 = new Comment { CommentId = 2 };
+            var comment3 = new Comment { CommentId = 3 };
+            var dict = new Dictionary<object, Post>();
+            var func = (Func<Post, Comment, Post>)funcFac.DynamicInvoke(dict);
+            func(post1, comment1);
+            func(post1, comment2);
+            func(post2, comment3);
+            Assert.Equal(1, dict[1].Comments.First().CommentId);
+            Assert.Equal(2, dict[1].Comments.Last().CommentId);
+            Assert.Equal(3, dict[2].Comments.First().CommentId);
+        }
 
-        ////private static Func<IDictionary<object, Post>, Delegate> GenerateSingleMapper() {
-        ////    var config = new CustomConfig();
-        ////    var selectQuery = new SelectQuery<Post>(config.Engine, new Mock<IDbConnection>().Object).Fetch(p => p.Comments) as SelectQuery<Post>;
-        ////    var writer = new SelectWriter(new SqlServer2012Dialect(), config);
-        ////    var result = writer.GenerateSql(selectQuery);
+        private static Delegate GenerateSingleMapper() {
+            var config = new CustomConfig();
+            var selectQuery = new SelectQuery<Post>(config.Engine, new Mock<IDbTransaction>().Object).Fetch(p => p.Comments) as SelectQuery<Post>;
+            var writer = new SelectWriter(new SqlServer2012Dialect(), config);
+            var result = writer.GenerateSql(selectQuery);
 
-        ////    var mapper = new DapperMapperGenerator(new Mock<IGeneratedCodeManager>().Object);
-        ////    var func = mapper.GenerateCollectionMapper<Post>(result.FetchTree, false);
-        ////    return func;
-        ////}
+            var mapper = new DapperMapperGenerator(GetMockCodeManager().Object);
+            var func = mapper.GenerateCollectionMapper<Post>(result.FetchTree, false);
+            return func;
+        }
 
-        ////class CustomConfig : DefaultConfiguration {
-        ////    public CustomConfig()
-        ////        : base(new System.Configuration.ConnectionStringSettings("Default", string.Empty, "System.Data.SqlClient")) {
-        ////        this.AddNamespaceOf<Post>();
-        ////    }
-        ////}
+        private static Mock<IGeneratedCodeManager> GetMockCodeManager() {
+            var mockCodeManager = new Mock<IGeneratedCodeManager>();
+            mockCodeManager.Setup(c => c.GetForeignKeyType(typeof(Post))).Returns(typeof(Post));
+            mockCodeManager.Setup(c => c.GetForeignKeyType(typeof(Blog))).Returns(typeof(Blog));
+            mockCodeManager.Setup(c => c.GetForeignKeyType(typeof(Comment))).Returns(typeof(Comment));
+            mockCodeManager.Setup(c => c.GetForeignKeyType(typeof(User))).Returns(typeof(User));
+            mockCodeManager.Setup(c => c.GetForeignKeyType(It.Is<Type>(t => t == typeof(Post)))).Returns(typeof(Post));
+            mockCodeManager.Setup(c => c.GetForeignKeyType(It.Is<Type>(t => t == typeof(Blog)))).Returns(typeof(Blog));
+            mockCodeManager.Setup(c => c.GetForeignKeyType(It.Is<Type>(t => t == typeof(Comment)))).Returns(typeof(Comment));
+            mockCodeManager.Setup(c => c.GetForeignKeyType(It.Is<Type>(t => t == typeof(User)))).Returns(typeof(User));
+            return mockCodeManager;
+        }
+
+        class CustomConfig : DefaultConfiguration {
+            public CustomConfig()
+                : base(new System.Configuration.ConnectionStringSettings("Default", string.Empty, "System.Data.SqlClient")) {
+                this.AddNamespaceOf<Post>();
+            }
+        }
     }
 }
