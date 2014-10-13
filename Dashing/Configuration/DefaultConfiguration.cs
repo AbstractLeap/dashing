b5d93aa2c78cea70e2614e834cd84ebdd716f5c4
Mark Jerzykowski <mark@jerzykowski.me.uk>
2014-10-13 10:49:48 +0100

    Added more overloads for DefaultConfiguration constructor

--- Dashing/Configuration/DefaultConfiguration.cs
+++ Dashing/Configuration/DefaultConfiguration.cs
@@ -8,6 +8,28 @@
 
     public class DefaultConfiguration : ConfigurationBase {
         public DefaultConfiguration(ConnectionStringSettings connectionStringSettings)
-            : base(new SqlEngine(new DialectFactory().Create(connectionStringSettings)), connectionStringSettings, DbProviderFactories.GetFactory(connectionStringSettings.ProviderName), new DefaultMapper(new DefaultConvention()), new DefaultSessionFactory(), new CodeGenerator(new CodeGeneratorConfig(), new ProxyGenerator(), new DapperWrapperGenerator())) { }
+            : this(connectionStringSettings, new DefaultConvention(), new CodeGeneratorConfig()) {
+        }
+
+        public DefaultConfiguration(ConnectionStringSettings connectionStringSettings, IConvention mappingConvention)
+            : this(connectionStringSettings, mappingConvention, new CodeGeneratorConfig()) {
+        }
+
+        public DefaultConfiguration(ConnectionStringSettings connectionStringSettings, CodeGeneratorConfig codeGeneratorConfig)
+            : this(connectionStringSettings, new DefaultConvention(), codeGeneratorConfig) {
+        }
+
+        public DefaultConfiguration(
+            ConnectionStringSettings connectionStringSettings,
+            IConvention mappingConvention,
+            CodeGeneratorConfig codeGeneratorConfig)
+            : base(
+                new SqlEngine(new DialectFactory().Create(connectionStringSettings)),
+                connectionStringSettings,
+                DbProviderFactories.GetFactory(connectionStringSettings.ProviderName),
+                new DefaultMapper(mappingConvention),
+                new DefaultSessionFactory(),
+                new CodeGenerator(codeGeneratorConfig, new ProxyGenerator(), new DapperWrapperGenerator())) {
+        }
     }
 }
