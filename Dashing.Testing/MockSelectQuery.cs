5e13f59312c2938b7a96fbc50511a2b28b225352
James Holwell <james@polylytics.com>
2015-05-13 12:04:45 +0100

    added extra method support to MockSelectQuery

--- Dashing.Testing/MockSelectQuery.cs
+++ Dashing.Testing/MockSelectQuery.cs
@@ -1,4 +1,7 @@
-﻿namespace Dashing.Testing {
+﻿// ReSharper disable CollectionNeverQueried.Global
+// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+namespace Dashing.Testing {
     using System;
     using System.Collections;
     using System.Collections.Generic;
@@ -8,38 +11,67 @@
     using System.Threading.Tasks;
 
     public class MockSelectQuery<T> : ISelectQuery<T> {
-        private IEnumerable<T> query;
-
-        private readonly IList<T> testList;
+        private readonly IList<T> allRecords;
 
         private readonly WhereClauseNullCheckRewriter whereClauseNullCheckRewriter;
 
-        public MockSelectQuery(IList<T> testList) {
-            this.testList = testList;
-            this.query = testList;
+        public MockSelectQuery(IList<T> allRecords) {
+            this.allRecords = allRecords;
             this.Fetches = new List<Expression>();
             this.CollectionFetches = new List<KeyValuePair<Expression, Stack<Expression>>>();
-            this.WhereClauses = new List<Expression>();
-            this.OrderClauses = new List<KeyValuePair<Expression, ListSortDirection>>();
+            this.WhereClauses = new List<Expression<Func<T, bool>>>();
+            this.OrderClauses = new List<Tuple<Expression, ListSortDirection, Func<IEnumerable<T>, IEnumerable<T>>>>();
             this.whereClauseNullCheckRewriter = new WhereClauseNullCheckRewriter();
         }
 
+        public IList<Expression> Includes { get; set; }
+
+        public IList<Expression> Excludes { get; set; }
+        
         public IList<Expression> Fetches { get; set; }
 
         public IList<KeyValuePair<Expression, Stack<Expression>>> CollectionFetches { get; private set; }
 
-        public IList<Expression> WhereClauses { get; set; }
+        public IList<Expression<Func<T, bool>>> WhereClauses { get; set; }
 
-        public IList<KeyValuePair<Expression, ListSortDirection>> OrderClauses { get; set; }
+        public IList<Tuple<Expression, ListSortDirection, Func<IEnumerable<T>, IEnumerable<T>>>> OrderClauses { get; set; }
 
         public bool IsForUpdate { get; set; }
 
         public bool IsTracked { get; set; }
 
-        public int TakeN { get; set; }
+        public bool IsIncludingAll { get; set; }
+
+        public int? TakeN { get; set; }
+        
+        public int? SkipN { get; set; }
+
+        private IEnumerable<T> Results {
+            get {
+                var q = this.allRecords.ToArray() as IEnumerable<T>;
+
+                foreach (var predicate in this.WhereClauses) {
+                    q = q.Where(this.whereClauseNullCheckRewriter.Rewrite(predicate).Compile());
+                }
+
+                foreach (var order in this.OrderClauses) {
+                    q = order.Item3(q);
+                }
+
+                if (this.SkipN.HasValue) {
+                    q = q.Skip(this.SkipN.Value);
+                }
+                
+                if (this.TakeN.HasValue) {
+                    q = q.Take(this.TakeN.Value);
+                }
+
+                return q;
+            }
+        }
 
         public IEnumerator<T> GetEnumerator() {
-            return this.testList.GetEnumerator();
+            return this.Results.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator() {
@@ -51,18 +83,22 @@
         }
 
         public ISelectQuery<T> IncludeAll() {
-            throw new NotImplementedException();
+            this.IsIncludingAll = true;
+            return this;
         }
-
+        
         public ISelectQuery<T> Include<TResult>(Expression<Func<T, TResult>> includeExpression) {
-            throw new NotImplementedException();
+            this.Includes.Add(includeExpression);
+            return this;
         }
 
         public ISelectQuery<T> Exclude<TResult>(Expression<Func<T, TResult>> excludeExpression) {
-            throw new NotImplementedException();
+            this.Excludes.Add(excludeExpression);
+            return this;
         }
 
         public IFetchMany<T, TResult> FetchMany<TResult>(Expression<Func<T, IEnumerable<TResult>>> selector) {
+            // the MockFetchMany updates CollectionFetches for us
             return new MockFetchMany<T, TResult>(selector, this);
         }
 
@@ -82,115 +118,127 @@
         }
 
         public ISelectQuery<T> AsNonTracked() {
-            throw new NotImplementedException();
+            this.IsTracked = false;
+            return this;
         }
 
         public ISelectQuery<T> Skip(int skip) {
-            throw new NotImplementedException();
+            this.SkipN = skip;
+            return this;
         }
 
         public ISelectQuery<T> Take(int take) {
             this.TakeN = take;
-            this.query = this.query.Take(take);
             return this;
         }
 
         public ISelectQuery<T> Where(Expression<Func<T, bool>> predicate) {
             this.WhereClauses.Add(predicate);
-            this.query = this.query.Where(this.whereClauseNullCheckRewriter.Rewrite(predicate).Compile());
             return this;
         }
 
         public ISelectQuery<T> OrderBy<TResult>(Expression<Func<T, TResult>> keySelector) {
-            this.OrderClauses.Add(new KeyValuePair<Expression, ListSortDirection>(keySelector, ListSortDirection.Ascending));
-            this.query = this.query.OrderBy(keySelector.Compile());
+            this.OrderClauses.Add(Tuple.Create<Expression, ListSortDirection, Func<IEnumerable<T>, IEnumerable<T>>>(keySelector, ListSortDirection.Ascending, q => q.OrderBy(keySelector.Compile())));
             return this;
         }
 
         public ISelectQuery<T> OrderByDescending<TResult>(Expression<Func<T, TResult>> keySelector) {
-            this.OrderClauses.Add(new KeyValuePair<Expression, ListSortDirection>(keySelector, ListSortDirection.Descending));
-            this.query = this.query.OrderByDescending(keySelector.Compile());
+            this.OrderClauses.Add(Tuple.Create<Expression, ListSortDirection, Func<IEnumerable<T>, IEnumerable<T>>>(keySelector, ListSortDirection.Descending, q => q.OrderByDescending(keySelector.Compile())));
             return this;
         }
 
         public T First() {
-            return this.query.First();
+            return this.Results.First();
         }
 
         public T First(Expression<Func<T, bool>> predicate) {
             this.WhereClauses.Add(predicate);
-            return this.query.First(this.whereClauseNullCheckRewriter.Rewrite(predicate).Compile());
+            return this.First();
         }
 
         public T FirstOrDefault() {
-            return this.query.FirstOrDefault();
+            return this.Results.FirstOrDefault();
         }
 
         public T FirstOrDefault(Expression<Func<T, bool>> predicate) {
             this.WhereClauses.Add(predicate);
-            return this.query.FirstOrDefault(this.whereClauseNullCheckRewriter.Rewrite(predicate).Compile());
+            return this.FirstOrDefault();
         }
 
         public T Single() {
-            return this.query.Single();
+            return this.Results.Single();
         }
 
         public T Single(Expression<Func<T, bool>> predicate) {
             this.WhereClauses.Add(predicate);
-            return this.Single(this.whereClauseNullCheckRewriter.Rewrite(predicate).Compile());
+            return this.Single();
         }
 
         public T SingleOrDefault() {
-            return this.query.SingleOrDefault();
+            return this.Results.SingleOrDefault();
         }
 
         public T SingleOrDefault(Expression<Func<T, bool>> predicate) {
             this.WhereClauses.Add(predicate);
-            return this.query.SingleOrDefault(this.whereClauseNullCheckRewriter.Rewrite(predicate).Compile());
+            return this.SingleOrDefault();
         }
 
         public T Last() {
-            throw new NotImplementedException();
+            return this.Results.Last();
         }
 
         public T Last(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            this.WhereClauses.Add(predicate);
+            return this.Last();
         }
 
         public T LastOrDefault() {
-            throw new NotImplementedException();
+            return this.Results.LastOrDefault();
         }
 
         public T LastOrDefault(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            this.WhereClauses.Add(predicate);
+            return this.LastOrDefault();
         }
 
         public int Count() {
-            throw new NotImplementedException();
+            return this.Results.Count();
         }
 
         public int Count(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            this.WhereClauses.Add(predicate);
+            return this.Count();
         }
 
         public Page<T> AsPaged(int skip, int take) {
-            throw new NotImplementedException();
+            this.SkipN = this.TakeN = null;
+            var totalResults = this.Count();
+            this.SkipN = skip;
+            this.TakeN = take;
+
+            return new Page<T> {
+                Items = this.Results.ToArray(),
+                Skipped = skip,
+                Taken = take,
+                TotalResults = totalResults
+            };
         }
 
         public bool Any() {
-            throw new NotImplementedException();
+            return this.Results.Any();
         }
 
         public bool Any(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            this.WhereClauses.Add(predicate);
+            return this.Any();
         }
 
         public Task<IList<T>> ToListAsync() {
-            return Task.FromResult((IList<T>)this.query.ToList());
+            return Task.FromResult((IList<T>)this.Results.ToList());
         }
 
         public Task<T[]> ToArrayAsync() {
-            return Task.FromResult(this.query.ToArray());
+            return Task.FromResult(this.Results.ToArray());
         }
 
         public Task<T> FirstAsync() {
@@ -226,39 +274,39 @@
         }
 
         public Task<T> LastAsync() {
-            throw new NotImplementedException();
+            return Task.FromResult(this.Last());
         }
 
         public Task<T> LastAsync(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            return Task.FromResult(this.Last(predicate));
         }
 
         public Task<T> LastOrDefaultAsync() {
-            throw new NotImplementedException();
+            return Task.FromResult(this.LastOrDefault());
         }
 
         public Task<T> LastOrDefaultAsync(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            return Task.FromResult(this.LastOrDefault(predicate));
         }
 
         public Task<int> CountAsync() {
-            throw new NotImplementedException();
+            return Task.FromResult(this.Count());
         }
 
         public Task<int> CountAsync(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            return Task.FromResult(this.Count(predicate));
         }
 
         public Task<Page<T>> AsPagedAsync(int skip, int take) {
-            throw new NotImplementedException();
+            return Task.FromResult(this.AsPaged(skip, take));
         }
 
         public Task<bool> AnyAsync() {
-            throw new NotImplementedException();
+            return Task.FromResult(this.Any());
         }
 
         public Task<bool> AnyAsync(Expression<Func<T, bool>> predicate) {
-            throw new NotImplementedException();
+            return Task.FromResult(this.Any(predicate));
         }
     }
 }
