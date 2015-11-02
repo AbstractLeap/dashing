701a870f743ae406e467be9d0343d80eb0b37f31
Mark Jerzykowski <mark.jerzykowski13@imperial.ac.uk>
2015-11-02 23:59:54 +0000

    A mock sessionfactory sounds like a good idea

---
+++ Dashing.Testing/MockSessionFactory.cs
@@ -0,0 +1,28 @@
+namespace Dashing.Testing
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Data;
+	
+	using Dashing;
+	using Dashing.Configuration;
+	using Dashing.Engine;
+	
+	public class MockSessionFactory : ISessionFactory {
+		private IDictionary<Type, IList> testLists;
+		
+		public MockSessionFactory() {}
+		
+		public MockSessionFactory(IDictionary<Type, IList> testLists) {
+			this.testLists = testLists;
+		}
+		
+		public ISession Create(IEngine engine, IDbConnection connection, IDbTransaction transaction = null, bool disposeConnection = true, bool commitAndDisposeTransaction = false, bool isTransactionLess = false) {
+			if (this.testLists != null) {
+				return new MockSession(this.testLists);
+			} else {
+				return new MockSession();
+			}
+		}
+	}
+}
