dc9591246b929ce4099e28dc41e2625410f93ae5
Mark Jerzykowski <mark@jerzykowski.me.uk>
2018-02-21 22:59:07 +0000

    extract configs is called correctly in netcore during weave

--- Dashing.Weaver/Weaving/ConfigurationWeaver.cs
+++ Dashing.Weaver/Weaving/ConfigurationWeaver.cs
@@ -56,7 +56,7 @@
                                        StartInfo = new ProcessStartInfo {
 #if COREFX
                                                                             FileName = "dotnet",
-                                                                            Arguments = $"{Path.GetFileNameWithoutExtension(typeof(ConfigurationWeaver).Assembly().Location)} extractconfigs " + arguments,
+                                                                            Arguments = $"{typeof(ConfigurationWeaver).Assembly().Location} extractconfigs " + arguments,
 #else
                                                                             FileName = Path.GetFileName(Assembly.GetExecutingAssembly().Location),
                                                                             Arguments = "extractconfigs " + arguments,
