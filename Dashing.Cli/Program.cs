460655373e6744fd04feca26508d6f6cf3d97f1b
Mark Jerzykowski <mark@polylytics.com>
2018-04-06 22:20:35 +0100

    Add weaving to csproj via dash

--- Dashing.Cli/Program.cs
+++ Dashing.Cli/Program.cs
@@ -5,6 +5,7 @@
     using System.IO;
     using System.Linq;
     using System.Reflection;
+    using System.Xml;
 
     using Dashing.Configuration;
     using Dashing.Extensions;
@@ -35,6 +36,7 @@
             ConfigureScript(app);
             ConfigureMigrate(app);
             ConfigureSeed(app);
+            ConfigureAddWeave(app);
 
             app.OnExecute(
                 () => {
@@ -242,6 +244,86 @@
                     : "System.Data.SqlClient");
         }
 
+        private static void ConfigureAddWeave(CommandLineApplication app) {
+            app.Command(
+                "addweave",
+                c => {
+                    c.Description = "Executes a function to add weaving to a project";
+                    c.HelpOption("-?|-h|--help");
+
+                    // attempts to weave the assemblies at the specified location
+                    var projectFilePath = c.Option("-p|--projectfilepath <path>", "Specify the path to the project file to which weaving should be added", CommandOptionType.SingleValue);
+                    var configurationType = c.Option("-c|--configurationtype <typefullname>", "The full name of the configuration type", CommandOptionType.SingleValue);
+                    var assemblyExtension = c.Option("-a|--assemblyextension <extension>", "The extension of the assembly (when it has been built, e.g. 'dll', 'exe')", CommandOptionType.SingleValue);
+
+                    c.OnExecute(
+                        () => {
+                            if (!projectFilePath.HasValue()) {
+                                Console.WriteLine("Please specify the path to the project file to which weaving should be added");
+                                return 1;
+                            }
+
+                            if (!configurationType.HasValue()) {
+                                Console.WriteLine("Please specify the configuration full name");
+                                return 1;
+                            }
+
+                            if (!assemblyExtension.HasValue()) {
+                                Console.WriteLine("Please specify the assembly extension e.g. 'dll', 'exe'");
+                                return 1;
+                            }
+
+                            var projectFileFullPath = Path.GetFullPath(projectFilePath.Value());
+                            assemblySearchDirectories.Insert(0, Path.GetDirectoryName(projectFileFullPath)); // favour user code over dashing code
+                            try {
+                                ExecuteAddWeave(projectFileFullPath, configurationType, assemblyExtension);
+                                return 0;
+                            } catch (Exception ex) {
+                                Console.WriteLine(ex.Message);
+                                return 1;
+                            }
+                        });
+                });
+        }
+
+        private static void ExecuteAddWeave(string projectFileFullPath, CommandOption configurationType, CommandOption assemblyExtension) {
+            if (!File.Exists(projectFileFullPath)) {
+                throw new Exception($"Project {projectFileFullPath} does not exist");
+            }
+
+            var xmlDocument = new XmlDocument();
+            using (var projectFileStream = File.OpenRead(projectFileFullPath)) {
+                xmlDocument.Load(projectFileStream);
+            }
+            
+            if (!string.Equals("Project", xmlDocument.DocumentElement.LocalName, StringComparison.OrdinalIgnoreCase)) {
+                throw new Exception("The project file must have a root element of <Project ...>");
+            }
+
+            var propertyGroup = xmlDocument.CreateElement("PropertyGroup");
+            var property = xmlDocument.CreateElement("WeaveArguments");
+            property.InnerText = $"-p \"$(MSBuildThisFileDirectory)$(OutputPath)$(AssemblyName).{assemblyExtension.Value()}\" -t \"{configurationType.Value()}\"";
+            propertyGroup.AppendChild(property);
+            var wasInserted = false;
+            for (var i = 0; i < xmlDocument.DocumentElement.ChildNodes.Count; i++) {
+                var childNode = xmlDocument.DocumentElement.ChildNodes[i];
+                if (string.Equals("Import", childNode.LocalName, StringComparison.OrdinalIgnoreCase)
+                    && (childNode.Attributes["Project"]?.Value?.ToUpperInvariant().Contains("DASHING") ?? false)) {
+                    xmlDocument.DocumentElement.InsertBefore(propertyGroup, childNode);
+                    wasInserted = true;
+                    break;
+                }
+            }
+
+            if (!wasInserted) {
+                xmlDocument.DocumentElement.InsertAfter(propertyGroup, xmlDocument.DocumentElement.LastChild);
+            }
+
+            using (var writeProjectStream = File.OpenWrite(projectFileFullPath)) {
+                xmlDocument.Save(writeProjectStream);
+            }
+        }
+
         private static IEnumerable<KeyValuePair<string, string>> GetExtraPluralizationWords(CommandOption extraPluralizationWords) {
             return (extraPluralizationWords.Values ?? Enumerable.Empty<string>()).Select(
                 s => {
@@ -322,7 +404,7 @@
                     }
                 }
 
-                return context.LoadFromAssemblyName(name);
+                return null;
             };
 #else
             assemblySearchDirectories = (ConfigurationManager.AppSettings["AssemblySearchPaths"]
