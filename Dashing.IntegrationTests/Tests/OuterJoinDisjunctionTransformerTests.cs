f194bdb2f9813a3dee3ed3fbd5f0382a019e5eb9
Mark Jerzykowski <mark@polylytics.com>
2019-05-29 17:14:34 +0100

    An integration test for non root disjunctions

---
+++ Dashing.IntegrationTests/Tests/OuterJoinDisjunctionTransformerTests.cs
@@ -0,0 +1,16 @@
+﻿namespace Dashing.IntegrationTests.Tests {
+    using Dashing.IntegrationTests.Setup;
+    using Dashing.IntegrationTests.TestDomain;
+
+    using Xunit;
+
+    public class OuterJoinDisjunctionTransformerTests {
+        [Theory]
+        [MemberData(nameof(SessionDataGenerator.GetSessions), MemberType = typeof(SessionDataGenerator))]
+        public void OuterJoinDisjunctionWorks(TestSessionWrapper wrapper) {
+            var posts = wrapper.Session.Query<Post>()
+                               .Where(p => p.Blog.Title == "Blog_1" || p.Blog.Title == "Blog_2");
+            Assert.NotEmpty(posts);
+        }
+    }
+}
