c4b1691954cbb8b148f73d8aa51fe304b1e99671
Mark Jerzykowski <mark@polylytics.com>
2019-11-01 21:19:11 +0000

    Few more tests for projections

--- Dashing.IntegrationTests/Tests/ProjectionTests.cs
+++ Dashing.IntegrationTests/Tests/ProjectionTests.cs
@@ -43,5 +43,38 @@
             Assert.NotNull(posts.Items.ElementAt(0).Title);
             Assert.NotNull(posts.Items.ElementAt(0).BlogTitle);
         }
+
+        [Theory]
+        [MemberData(nameof(SessionDataGenerator.GetSessions), MemberType = typeof(SessionDataGenerator))]
+        public void FetchSinglePropWorks(TestSessionWrapper wrapper) {
+            var posts = wrapper.Session.Query<Post>()
+                               .Select(p => p.PostId)
+                               .AsPaged(10, 10);
+            Assert.Equal(10, posts.Taken);
+            Assert.True(posts.TotalResults > 10);
+        }
+
+        [Theory]
+        [MemberData(nameof(SessionDataGenerator.GetSessions), MemberType = typeof(SessionDataGenerator))]
+        public void FetchRelationPropWorks(TestSessionWrapper wrapper) {
+            var blogs = wrapper.Session.Query<Post>()
+                               .Select(p => p.Blog)
+                               .AsPaged(10, 10);
+            Assert.Equal(10, blogs.Taken);
+            Assert.True(blogs.TotalResults > 10);
+            Assert.NotNull(
+                blogs.Items.First()
+                     .Title);
+        }
+
+        [Theory]
+        [MemberData(nameof(SessionDataGenerator.GetSessions), MemberType = typeof(SessionDataGenerator))]
+        public void FetchRelationIdWorks(TestSessionWrapper wrapper) {
+            var blogs = wrapper.Session.Query<Post>()
+                               .Select(p => p.Blog.BlogId)
+                               .AsPaged(10, 10);
+            Assert.Equal(10, blogs.Taken);
+            Assert.True(blogs.TotalResults > 10);
+        }
     }
 }
