04c2d10b256d5bedd25cb07a9063d6f629d7687a
Mark Jerzykowski <mark@polylytics.com>
2020-02-27 21:05:51 +0000

    moving towards supporting various frameworks

--- Package Dashing.bat
+++ Package Dashing.bat
@@ -1,6 +1,6 @@
 @echo off
 set /p version="What is the package version? "
-dotnet pack ./Dashing/Dashing.csproj --output ../ --configuration Release /p:PackageVersion=%version%
-dotnet pack ./Dashing.Cli/Dashing.Cli.csproj --output ../ --configuration Release /p:PackageVersion=%version%
-dotnet pack ./Dashing.Weaver/Dashing.Weaver.csproj --output ../ --configuration Release /p:PackageVersion=%version%
+dotnet pack ./Dashing/Dashing.csproj --output ./ --configuration Release /p:PackageVersion=%version%
+dotnet pack ./Dashing.Cli/Dashing.Cli.csproj --output ./ --configuration Release /p:PackageVersion=%version%
+dotnet pack ./Dashing.Weaver/Dashing.Weaver.csproj --output ./ --configuration Release /p:PackageVersion=%version%
 pause
