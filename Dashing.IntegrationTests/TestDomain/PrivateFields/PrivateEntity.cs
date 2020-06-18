97c6e2d929d8309fdfe5ba9d445213b590a6fbcd
Mark Jerzykowski <mark@polylytics.com>
2020-06-18 21:01:12 +0100

    Test that dapper maps to private fields

---
+++ Dashing.IntegrationTests/TestDomain/PrivateFields/PrivateEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Dashing.IntegrationTests.TestDomain.PrivateFields {
+    using System;
+
+    public class PrivateEntity {
+        private Guid id;
+
+        private string name;
+
+        public override string ToString() {
+            return $"{id}:{name}";
+        }
+    }
+}
--- Dashing.IntegrationTests/Tests/DapperWrapperTests.cs
+++ Dashing.IntegrationTests/Tests/DapperWrapperTests.cs
@@ -1,12 +1,21 @@
 ﻿namespace Dashing.IntegrationTests.Tests {
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
 
     using Dashing.IntegrationTests.Setup;
+    using Dashing.IntegrationTests.TestDomain.PrivateFields;
 
     using Xunit;
+    using Xunit.Abstractions;
 
     public class DapperWrapperTests {
+        private readonly ITestOutputHelper output;
+
+        public DapperWrapperTests(ITestOutputHelper output) {
+            this.output = output;
+        }
+
         [Theory]
         [MemberData(nameof(SessionDataGenerator.GetSessions), MemberType = typeof(SessionDataGenerator))]
         public async Task DynamicQueryAsyncWorks(TestSessionWrapper wrapper) {
@@ -22,6 +31,20 @@
             Assert.Single(result);
             Assert.Equal(1, result.First().FooId);
             Assert.Equal("Rah", result.First().Bar);
+        }
+
+        [Fact]
+        public async Task PrivateFieldsWorkInDapper() {
+            var config = new Configuration();
+            var database = new SqlServerDatabase(config);
+            using (var session = database.BeginSession()) {
+                var id = Guid.NewGuid();
+                var privateEntity = await session.Dapper.QueryAsync<PrivateEntity>($"select cast('{id}' as uniqueidentifier) as Id, 'Foo' as name");
+                Assert.Equal(
+                    $"{id}:Foo",
+                    privateEntity.First()
+                                 .ToString());
+            }
         }
 
         public class Foo {
